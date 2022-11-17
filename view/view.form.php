30606977236e3777cf647843f128ed18d2f87156
Cpradlls <pradeilles.chloe@gmail.com>
2022-11-17 11:49:13 +0100

    commentaire vérif phone view form

--- view/view.form.php
+++ view/view.form.php
@@ -46,7 +46,7 @@
             echo '</div>'; */
 
             echo '<div class="form-checkbox pb-2">';
-            if ($oCustomer->isOptIn_phone == 0 || $oCustomer->isOptIn_phone == null && $oCustomer->isOptIn_email == 0 || $oCustomer->isOptIn_email == null && $oCustomer->isOptIn_mail == 0 || $oCustomer->isOptIn_mail == null && $oCustomer->isOptIn_sms == 0 || $oCustomer->isOptIn_sms == null){
+            if (/* $oCustomer->isOptIn_phone == 0 || */ $oCustomer->isOptIn_phone == null && $oCustomer->isOptIn_email == 0 || $oCustomer->isOptIn_email == null && $oCustomer->isOptIn_mail == 0 || $oCustomer->isOptIn_mail == null && $oCustomer->isOptIn_sms == 0 || $oCustomer->isOptIn_sms == null){
                 echo '<input type="checkbox" name="opt_in_none" id="opt_in_none" class="form-check-input rose" checked>';
             } else {
                 echo '<input type="checkbox" name="opt_in_none" id="opt_in_none" class="form-check-input" >';
